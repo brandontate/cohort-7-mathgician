d89d6acc231fd9392cdd1e9d7f2e7cc4a8ce0c77
brandontate <blt900@gmail.com>
2015-01-16 15:43:59 -0600

    Initializing Project

---
+++ Mathgician/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathgician
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am the Mathgician");
+            Console.WriteLine("What shall I math?\nFibonacci, Primes, or Integers");
+            string desiredMaths = Console.ReadLine();
+            Console.WriteLine("Ok. I'm gonna do some " + desiredMaths);
+            if( "Integers" == desiredMaths)
+            {
+                //Do something here
+            }
+            else
+            {
+                Console.WriteLine("Well... Actually I lied");
+            }
+            System.Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}

e4b225b70ef813490ce8e0c9196e07810f4144da
brandontate <blt900@gmail.com>
2015-01-18 21:28:19 -0600

    Primes added

--- Mathgician/Program.cs
+++ Mathgician/Program.cs
@@ -23,12 +23,49 @@ namespace Mathgician
             {
                 PringFibonacci();
             }
+            else if ("Primes" == desiredMaths)
+            {
+                PrintPrimes();
+            }
             else
             {
                 Console.WriteLine("Well... Actually I lied");
             }
             System.Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+        }
+
+        private static void PrintPrimes()
+        {
+            int i = 2;
+            var NumbersToCheck = new List<int> { };
+            while (true)
+            {
+                bool found = false;
+                for (int y = i - 1; y > 1; y--)
+                {
+                    if (i % y == 0)
+                    {
+                        found = true;
+                    }
+                }
+                foreach (int number in NumbersToCheck)
+                {
+                    int answer = (i / number) % 1;
+                    if (answer != 0)
+                    {
+                        found = true;
+                    }
+
+                }
+                if (found == false) 
+                {
+                    Console.WriteLine(i);
+                    System.Threading.Thread.Sleep(25);
+                    NumbersToCheck.Add(i);
+                }
+                i++;
+            }
         }
 
         private static void PringFibonacci()
